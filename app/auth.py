ffaecdc6e3ad517d7c97a832f75a7eb84b8d7201
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-28 18:51:59 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,24 +1,13 @@
 from passlib.context import CryptContext
-from jose import jwt, JWTError
-from datetime import datetime, timedelta
-from fastapi import Request
-from app.config import SECRET_KEY
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-ALGORITHM = "HS256"
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
-def hash_password(p): return pwd_context.hash(p)
-def verify_password(p,h): return pwd_context.verify(p,h)
+def get_password_hash(password: str):
+    # bcrypt safe limit
+    return pwd_context.hash(password[:72])
 
-def create_token(data):
-    expire = datetime.utcnow() + timedelta(hours=12)
-    data.update({"exp": expire})
-    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
-
-def get_current_user(request: Request):
-    token = request.cookies.get("access_token")
-    if not token: return None
-    try:
-        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    except JWTError:
-        return None
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
