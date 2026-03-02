5732fd190446f3dc78ffa2cbd0151068b005973e
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-02 14:48:18 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,35 +1,19 @@
-from passlib.context import CryptContext
-import jwt
 import os
-from datetime import datetime, timedelta
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not set")
 
-if not SECRET_KEY:
-    raise Exception("SECRET_KEY not set")
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
-def get_password_hash(password: str):
-    if not password:
-        raise ValueError("Password cannot be empty")
-
-    password = str(password)
-    password = password.encode("utf-8")[:72].decode("utf-8", errors="ignore")
-
-    return pwd_context.hash(password)
-
-def verify_password(plain, hashed):
-    plain = str(plain)
-    plain = plain.encode("utf-8")[:72].decode("utf-8", errors="ignore")
-    return pwd_context.verify(plain, hashed)
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(days=1)
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
