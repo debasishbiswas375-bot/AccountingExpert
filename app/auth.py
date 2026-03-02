a9dbe41558f8f3774675481f989b2718e2602319
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-02 14:02:54 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-pwd_context = CryptContext(schemes=[""bcrypt""], deprecated=""auto"")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-SECRET_KEY = os.getenv(""SECRET_KEY"")
-ALGORITHM = ""HS256""
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
 
 def get_password_hash(password: str):
     return pwd_context.hash(password[:72])
@@ -20,5 +20,6 @@ def verify_password(plain, hashed):
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(days=1)
-    to_encode.update({""exp"": expire})
+    to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
