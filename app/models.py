6ab517e819b1982a8207c957a8109b8414e856a4
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-06 15:36:54 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -21,6 +21,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+
+    username = Column(String(50), unique=True, index=True, nullable=False)
+
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=True)
 
