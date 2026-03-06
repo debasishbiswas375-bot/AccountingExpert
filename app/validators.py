bfc99e26cefcf0756731af6372765486fc566a9b
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-06 18:18:23 +0530

    Delete app/validators.py

--- app/validators.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def validate_username(username: str):
-    if len(username) < 6:
-        raise ValueError("Username must be at least 6 characters")
-
-    if not re.match("^[A-Za-z0-9]+$", username):
-        raise ValueError("Username must contain only letters and numbers")
