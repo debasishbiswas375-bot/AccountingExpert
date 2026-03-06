e651212c13932163c168b90c3a979532c2401c92
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-06 18:03:17 +0530

    Update auth_routes.py

--- app/routers/auth_routes.py
+++ app/routers/auth_routes.py
@@ -1,1 +1,22 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 
+router = APIRouter()
+
+templates = Jinja2Templates(directory="app/templates")
+
+
+@router.get("/login")
+def login_page(request: Request):
+    return templates.TemplateResponse(
+        "login.html",
+        {"request": request}
+    )
+
+
+@router.get("/register")
+def register_page(request: Request):
+    return templates.TemplateResponse(
+        "register.html",
+        {"request": request}
+    )
