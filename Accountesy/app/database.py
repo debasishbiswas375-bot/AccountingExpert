3e52cbd36acc0dede6efb6f64c46a2ea9503afbc
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-16 16:23:52 +0530

    Update database.py

--- Accountesy/app/database.py
+++ Accountesy/app/database.py
@@ -1,8 +1,13 @@
-﻿import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import os
+from supabase import create_client
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine)
+if not SUPABASE_URL:
+    raise Exception("SUPABASE_URL missing")
+
+if not SUPABASE_KEY:
+    raise Exception("SUPABASE_KEY missing")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
