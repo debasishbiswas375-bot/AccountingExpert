55859a7c0b5ee90220e2a8024f5fc7cd20d79957
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-19 16:37:36 +0530

    Delete Accountesy directory

--- Accountesy/app/routers/converter.py
+++
@@ -1,9 +0,0 @@
-﻿from fastapi import APIRouter, UploadFile, File
-from app.logic.processor import process_statement
-
-router = APIRouter()
-
-@router.post("/convert")
-async def convert(statement: UploadFile = File(...), master: UploadFile | None = File(None)):
-    return process_statement(statement, master)
-
